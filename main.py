cf03470baad08897cd5dd8c3281aff59d52c6b22
Han <puhoi125@mil.ru>
2024-10-01 16:16:15 +0300

    Initial commit1

---
+++ main.py
@@ -0,0 +1,9 @@
+my_string=input('владение и право квартиры ')
+print(len(my_string))
+
+print('владениеи право квартиры ' .upper())
+print('владениеи право квартиры '.lower())
+print('владениеи право квартиры ' .replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
+
